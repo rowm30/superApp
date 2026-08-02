63170a70fc76d166c1ed82576de354ea129c6b16
kmv9063 <rowm30@gmail.com>
2026-08-02 14:46:48 +0400

    feat(edge-gateway): reactive gateway routing to profile-service

---
+++ services/edge-gateway/src/main/java/com/superapp/edge_gateway/EdgeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.superapp.edge_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeGatewayApplication.class, args);
+	}
+
+}
---
+++ services/edge-gateway/src/test/java/com/superapp/edge_gateway/EdgeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superapp.edge_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
